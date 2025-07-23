0e55b920260894c20cf7e71a8724824c64d3ac8f
NathanSchittl <nathanschittl@gmail.com>
2025-07-22 18:12:12 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,14 +6,6 @@ import { getStorage } from "firebase/storage";
 
 // TODO: Replace with your Firebase project configuration (from Firebase console)
 // You can find these under Project Settings → General → Your apps.
-const firebaseConfig = {// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyClDU6x5KzOcYQC09OjwNQFkMhNiBYuKjc",
   authDomain: "sitesnap-28da9.firebaseapp.com",
@@ -22,11 +14,6 @@ const firebaseConfig = {
   messagingSenderId: "230487344341",
   appId: "1:230487344341:web:607b67f8e6263d86ba0e5c",
   measurementId: "G-MXH9N9N99Q"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 };
 
 const app = initializeApp(firebaseConfig);
